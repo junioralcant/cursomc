81a52900daec0446944e8c9f40e0ab27f09fd739
Junior Marques <junioralcant111@gmail.com>
2018-07-09 11:37:27 -0300

    Criando repository e service para categoria

--- src/main/java/com/webtolls/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/webtolls/cursomc/resources/CategoriaResource.java
@@ -1,26 +1,33 @@
 package com.webtolls.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.webtolls.cursomc.domain.Categoria;
+import com.webtolls.cursomc.services.CategoriaService;
 
 @RestController 
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
-	@RequestMapping(method=RequestMethod.GET) //PARA SER UMA FUNÇÃO REST TEM Q SER ASSOCIADA A ALGUM VERBO HTTP
-	public List<Categoria>  listar() {
-		Categoria cat1 = new Categoria(1, "Informática");
-		Categoria cat2 = new Categoria(2, "Escritório");
-		
-		List<Categoria> lista = new ArrayList<>();
-		lista.add(cat1);
-		lista.add(cat2);
-		return lista;
+	@Autowired //INSTANCIA AUTOMATICAMENTE O OBJ
+	private CategoriaService service;
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET) //PARA SER UMA FUNÇÃO REST TEM Q SER ASSOCIADA A ALGUM VERBO HTTP
+	public ResponseEntity<?>  find(@PathVariable Integer id) {
+		Categoria obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);
 	}
+	
+	//ResponseEntity<?>: Armazena e encapsula varias informações de uma resposta HTTP para o tipo REST. a "?" quer dizer que pode ser qualquer tipo.
+	//value="/{id}": Informa que na URL vai ser acrescentato o ID da categoria EX: /categorias/1  
+	//@PathVariable Integer id: Informa que o id irformado no parametro vaiser redirecionado para a URL
+	//ResponseEntity.ok(): Da um ok se tudo ocorrer bem 
+	//.body(obj): Informa que o corpo do metodo vai ser a variável obj
 }
