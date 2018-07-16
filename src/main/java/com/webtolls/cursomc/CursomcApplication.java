b098312b0e50b5855a7baadd56c7bed9b8854de3
Junior Marques <junioralcant111@gmail.com>
2018-07-16 09:59:34 -0300

    Criando operacao de instaciacao

--- src/main/java/com/webtolls/cursomc/CursomcApplication.java
+++ src/main/java/com/webtolls/cursomc/CursomcApplication.java
@@ -1,12 +1,33 @@
 package com.webtolls.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.webtolls.cursomc.domain.Categoria;
+import com.webtolls.cursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner  {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null,"Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2)); // salva uma lista de objetos
+	}
+	
+	//CommandLineRunner: uma interface que permite chamar o metodos run que permite executar uma ação desejada 
 }
